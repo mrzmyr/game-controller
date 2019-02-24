a890e81aff938a8aea7ca66dcaba47afde999b48
mrzmyr <m0ritz.m3yer@gmail.com>
2019-02-24 01:20:35 +0100

    align colors

--- src/colors.js
+++ src/colors.js
@@ -1,17 +1,18 @@
-export const COLOR_BG = '#222831'
-export const COLOR_WALLS = '#393e46'
-export const COLOR_PLAYER = '#30e3ca'
+export const COLOR_BG = '#131313'
+export const COLOR_WALLS = '#1b1b1b'
+export const COLOR_PLAYER = '#ff0040'
 
-export const COLOR_DOORS = '#555a68'
-export const COLOR_DOORS_UNLOCKED = '#7e8495'
+export const COLOR_DOORS = '#272727'
+export const COLOR_DOORS_UNLOCKED = '#3d3d3d'
 
-export const COLOR_PLAYER_ULTIMATE_BODY = '#ff2e63'
-export const COLOR_PLAYER_ULTIMATE_BULLET = '#ff7e9e'
-export const COLOR_PLAYER_FATPILL = '#9c2a70'
+export const COLOR_PLAYER_ULTIMATE_BODY = '#f68187'
+export const COLOR_PLAYER_ULTIMATE_BULLET = '#f5555d'
+export const COLOR_PLAYER_FATPILL = '#7a09fa'
 
-export const COLOR_ITEM_LIFE = '#e64539'
-export const COLOR_ITEM_FATPILL = COLOR_PLAYER_FATPILL
+export const COLOR_ITEM_LIFE = '#c42430'
+export const COLOR_ITEM_FATPILL = '#7a09fa'
+export const COLOR_ITEM_BIGSHOT = '#5ac54f'
 
-export const COLOR_ENEMY_SHOOTER = '#f6416c'
-export const COLOR_ENEMY_STATION = '#f5ffe8'
+export const COLOR_ENEMY_SHOOTER = '#94fdff'
+export const COLOR_ENEMY_STATION = '#e07438'
 export const COLOR_ENEMY_DUBBER = '#FFF'
