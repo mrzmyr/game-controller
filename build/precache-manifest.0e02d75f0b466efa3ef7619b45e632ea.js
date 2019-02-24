1b53c515cdf4a66a95cb56b7af4d3af96b31ca1c
mrzmyr <m0ritz.m3yer@gmail.com>
2019-02-24 02:01:19 +0100

    add build

---
+++ build/precache-manifest.0e02d75f0b466efa3ef7619b45e632ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7af9c19ba7aad783c73",
+    "url": "/static/css/main.644845af.chunk.css"
+  },
+  {
+    "revision": "c7af9c19ba7aad783c73",
+    "url": "/static/js/main.c7af9c19.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5804a20ffb28b13a5d0f",
+    "url": "/static/js/2.5804a20f.chunk.js"
+  },
+  {
+    "revision": "0532cfb23d2aafae51577eaf328da5b4",
+    "url": "/static/media/heart.0532cfb2.svg"
+  },
+  {
+    "revision": "29c71f602d04b6ec6bb6f63ec5bc6d93",
+    "url": "/static/media/pill.29c71f60.svg"
+  },
+  {
+    "revision": "4a3ce195dd834d8b6965fbc46cc6cc3f",
+    "url": "/index.html"
+  }
+];
